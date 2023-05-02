b4e20da84a7bbca24d4414c45ff824e4fcf91cae
Hukasx0 <hubertkasp13@gmail.com>
2023-05-02 14:16:08 +0200

    Added proper response code handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use std::{collections::HashMap, fs::File, env, process};
+use ureq::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Request {
@@ -31,18 +32,22 @@ fn main() {
             process::exit(1);
         }
     };
+    let mut responses: Vec<ureq::Response> = Vec::new();
     for request in request_contents {
         match request.method.as_str() {
            "GET" => {
                 let url = format!("http://{}:{}{}", request.target, request.port, request.endpoint);
                 let response = match ureq::get(&url).call() {
                     Ok(r) => r,
+                    Err(Error::Status(_, r)) => r,
                     Err(e) => {
                         eprintln!("Error while connecting to {}:{}: {}", request.target, request.port, e);
                         process::exit(1);
                     }
                 };
                 println!("{:?}", response);
+                
+                responses.push(response);
            },
            "POST" => {
                 let url = format!("http://{}:{}{}", request.target, request.port, request.endpoint);
@@ -55,22 +60,59 @@ fn main() {
                 if let Some(data) = request.data {
                     match req.header("Content-Type") {
                         Some(content_type) if content_type.starts_with("application/x-www-form-urlencoded") => {
-                            let response = req.send_form(&data.iter().map(|(x, y)| (x.as_str(), y.as_str())).collect::<Vec<_>>()).unwrap();
-                            println!("{:?}", response);
+                            match req.send_form(&data.iter().map(|(x, y)| (x.as_str(), y.as_str())).collect::<Vec<_>>()) {
+                                Ok(r) => {
+                                    println!("{:?}", r);
+                                    responses.push(r);
+                                },
+                                Err(Error::Status(_, e)) => {
+                                    println!("{:?}", e);
+                                    responses.push(e);
+                                },
+                                Err(e) => {
+                                    eprintln!("{}", e);
+                                    break;
+                                },
+                            }
                         },
                         Some(content_type) if content_type.starts_with("application/json") => {
-                            let response = req.send_string(&serde_json::to_string(&serde_json::to_value(data).unwrap()).unwrap()).unwrap();
-                            println!("{:?}", response);
+                            match req.send_string(&serde_json::to_string(&serde_json::to_value(data).unwrap()).unwrap()) {
+                                Ok(r) => {
+                                    println!("{:?}", r);
+                                    responses.push(r);
+                                },
+                                Err(Error::Status(_, e)) => {
+                                    println!("{:?}", e);
+                                    responses.push(e);
+                                },
+                                Err(e) => {
+                                    eprintln!("{}", e);
+                                    break;
+                                },
+                            }
                         },
                         Some(content_type) if content_type.starts_with("text/plain") => {
-                            let response = req.send_string(&data.get("txt").map(|v| v.to_string()).unwrap());
-                            println!("{:?}", response);
+                            match req.send_string(&data.get("txt").map(|v| v.to_string()).unwrap()) {
+                                Ok(r) => {
+                                    println!("{:?}", r);
+                                    responses.push(r);
+                                },
+                                Err(Error::Status(_, e)) => {
+                                    println!("{:?}", e);
+                                    responses.push(e);
+                                },
+                                Err(e) => {
+                                    eprintln!("{}", e);
+                                    break;
+                                },
+                            }
                         },
                         Some(_) | None => {}
                     }
                 }
            },
-           _ => println!("other request") ,
+           _ => println!("Not supported request type") ,
         }
     }
+  //  println!("{:?}", responses);
 }
